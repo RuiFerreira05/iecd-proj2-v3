023437e8ff7a13bd694195a5b9a887e646ff3706
Rui Ferreira <166267260+RuiFerreira05@users.noreply.github.com>
2025-07-01 15:58:06 +0100

    // removed useless imports

--- src/main/java/client/ClientBean.java
+++ src/main/java/client/ClientBean.java
@@ -3,16 +3,9 @@ package client;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.StringReader;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.xml.sax.InputSource;
-
-import org.w3c.dom.Document;
 
 
 
